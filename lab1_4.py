5bbc28a4ab4d5607fc115a9f35aed7d9c1bb4187
weltyNic25 <wh977c2qjz@privaterelay.appleid.com>
2026-01-18 06:48:01 +0000

    Completed Lab one

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,6 @@
-pass
+def calculate_average(num1, num2,  num3):
+    return (num1+num2+num3)/3
+def add_tax(bill_total):
+    return(bill_total*1.10)
+def greet_user(name):
+    return("Hello "+name)
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,20 @@
-pass
+def check_multiple(number):
+    return number %3 == 0 and number %5 == 0
+
+
+def check_password(input_string):
+    if input_string == "Python123":
+        return "access granted"
+    else:
+        return "access denied"
+
+
+def calculate_federal_tax(salary):
+    if salary <= 11000:
+        return salary * 0.10
+    elif salary <= 44725:
+        return salary * 0.12
+    elif salary <= 95375:
+        return salary * 0.22
+    else:
+        return salary * 0.24
